db3dc5ee0b28cb9ea3bf8db821a39221d9c47d12
James Cranston <jcranston@cibr.com.au>
2021-04-26 09:22:46 +1000

    initial commit

---
+++ RaicovPC WPM clean install.ps1
@@ -0,0 +1,141 @@
+﻿
+winget install Google.Chrome | Out-Null
+if( $? ){
+    Write-Output "Google Chrome installed successfully"
+}
+
+winget install Google.DriveFileStream | Out-Null
+if( $? ){
+    Write-Output "Google Drive for Windows installed successfully"
+}
+
+winget install Microsoft.Office | Out-Null
+if( $? ){
+    Write-Output "Microsoft Office installed successfully"
+}
+
+winget install Microsoft.Teams | Out-Null
+if( $? ){
+    Write-Output "Microsoft Teams installed successfully"
+}
+
+winget install Microsoft.PowerToys | Out-Null
+if( $? ){
+    Write-Output "Powertoys installed successfully"
+}
+
+winget install Microsoft.WindowsTerminal | Out-Null 
+if( $? ){
+    Write-Output "Windows Terminal installed successfully"
+}
+
+winget install 7zip.7zip | Out-Null
+if( $? ){
+    Write-Output "7Zip installed successfully"
+}
+
+winget install VideoLAN.VLC | Out-Null
+if( $? ){
+    Write-Output "VLC installed successfully"
+}
+
+
+winget install Microsoft.VisualStudioCode.System-x64 | Out-Null
+if( $? ){
+    Write-Output "VS Code installed successfully"
+}
+
+winget install Nvidia.GeForceExperience | Out-Null
+if( $? ){
+    Write-Output "Nvidia Geforce Experience installed successfully"
+}
+
+winget install EpicGames.EpicGamesLauncher | Out-Null
+if( $? ){
+    Write-Output "Epic Games Launcher installed successfully"
+}
+
+winget install ElectronicArts.EADesktop | Out-Null
+if( $? ){
+    Write-Output "Origin installed successfully"
+}
+
+winget install Valve.Steam | Out-Null
+if( $? ){
+    Write-Output "Steam installed successfully"
+}
+
+winget install Blizzard.BattleNet | Out-Null
+if( $? ){
+    Write-Output "Battle.net installed successfully"
+}
+
+winget install GOG.Galaxy | Out-Null
+if( $? ){
+    Write-Output "GOG Galaxy installed successfully"
+}
+
+winget install Logitech.LGH | Out-Null
+if( $? ){
+    Write-Output "Logitech Gaming Hub installed successfully"
+}
+
+winget install Discord.Discord | Out-Null
+if( $? ){
+    Write-Output "Discord installed successfully"
+}
+
+winget install plex.Plex | Out-Null
+if( $? ){
+    Write-Output "Plex installed successfully"
+}
+
+winget install NZXT.CAM | Out-Null
+if( $? ){
+    Write-Output "NZXT CAM installed successfully"
+}
+
+winget install Surfshark.SurfsharkVPN | Out-Null
+if( $? ){
+    Write-Output "Surfshark installed successfully"
+}
+
+winget install Google.AndroidStudio | Out-Null 
+if( $? ){
+    Write-Output "Android Studio installed successfully"
+}
+
+winget install Git.Git | Out-Null
+if( $? ){
+    Write-Output "Git installed successfully"
+}
+
+winget install DelugeTeam.Deluge | Out-Null
+if( $? ){
+    Write-Output "Deluge installed successfully"
+}
+
+winget install TimKosse.FilezillaClient | Out-Null
+if( $? ){
+    Write-Output "Filezilla installed successfully"
+}
+
+winget install PuTTY.PuTTY | Out-Null
+if( $? ){
+    Write-Output "Putty installed successfully"
+}
+
+winget install Postman.Postman | Out-Null
+if( $? ){
+    Write-Output "Postman installed successfully"
+}
+
+winget install ApacheFriends.Xampp | Out-Null
+if( $? ){
+    Write-Output "Xampp installed successfully"
+}
+
+winget install WinSCP.WinSCP | Out-Null
+if( $? ){
+    Write-Output "WinSCP installed successfully"
+}
