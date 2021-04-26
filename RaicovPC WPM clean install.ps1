ff76f7fdabd024267be47ea7b06a712d526ae33d
raicovx <james.cranston91@gmail.com>
2021-04-26 11:16:23 +1000

    Organised and started implementing drive letter picker

--- RaicovPC WPM clean install.ps1
+++ RaicovPC WPM clean install.ps1
@@ -1,6 +1,16 @@
-﻿#Some Discussion about Out-Null / Pipe performance
+﻿#Some Discussion about Out-Null / Pipe performance - removed pipes to shave some milliseconds
 #https://stackoverflow.com/questions/5260125/whats-the-better-cleaner-way-to-ignore-output-in-powershell
 
+$DriveLetter = Read-Host -Prompt: 'Enter Destination Drive Letter'
+if ($DriveLetter.Length -gt 1 -or $DriveLetter.Length -eq 0)  {
+    Write-Output "Drive Letter should be a single letter"
+    Break Script
+} 
+$installPath = "$($DriveLetter):\Program Files\"
+
+#--------------------------
+# Browsers
+#--------------------------
 Out-Null -InputObject (winget install --id=Google.Chrome -e -h)
 if( $? ){
     Write-Output "Google Chrome installed successfully"
@@ -11,11 +21,9 @@ if( $? ){
     Write-Output "Vivaldi installed successfully"
 }
 
-Out-Null -InputObject (winget install --id=Google.DriveFileStream -e -h)
-if( $? ){
-    Write-Output "Google Drive for Windows installed successfully"
-}
-
+#--------------------------
+# Productivity
+#--------------------------
 Out-Null -InputObject (winget install --id=Microsoft.Office -e -h)
 if( $? ){
     Write-Output "Microsoft Office installed successfully"
@@ -26,26 +34,9 @@ if( $? ){
     Write-Output "Microsoft Teams installed successfully"
 }
 
-Out-Null -InputObject (winget install --id=Microsoft.PowerToys -e -h)
-if( $? ){
-    Write-Output "Powertoys installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=Microsoft.WindowsTerminal -e -h) 
-if( $? ){
-    Write-Output "Windows Terminal installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=7zip.7zip -e -h)
-if( $? ){
-    Write-Output "7Zip installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=VideoLAN.VLC -e -h)
-if( $? ){
-    Write-Output "VLC installed successfully"
-}
-
+#--------------------------
+# Development
+#--------------------------
 Out-Null -InputObject (winget install --id=Microsoft.VisualStudioCode.System-x64 -e -h)
 if( $? ){
     Write-Output "VS Code installed successfully"
@@ -56,6 +47,47 @@ if( $? ){
     Write-Output "Visual Studio Enterprise installed successfully"
 }
 
+Out-Null -InputObject (winget install --id=Google.AndroidStudio -e -h) 
+if( $? ){
+    Write-Output "Android Studio installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=JetBrains.IntelliJIDEA.Community -e -h)
+if( $? ){
+    Write-Output "IntelliJIDEA Community installed successfully"
+} 
+
+Out-Null -InputObject (winget install --id=Git.Git -e -h)
+if( $? ){
+    Write-Output "Git installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=OpenJS.NodeJS -e -h)
+if( $? ){
+    Write-Output "Node.JS installed successfully"
+} 
+
+Out-Null -InputObject (winget install --id=Postman.Postman -e -h)
+if( $? ){
+    Write-Output "Postman installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=ApacheFriends.Xampp -e -h)
+if( $? ){
+    Write-Output "Xampp installed successfully"
+}
+
+#--------------------------
+# Game Development
+#--------------------------
+Out-Null -InputObject (winget install --id=BlenderFoundation.Blender -e -h)
+if( $? ){
+    Write-Output "Blender installed successfully"
+} 
+
+#--------------------------
+# Gaming
+#--------------------------
 Out-Null -InputObject (winget install --id=Nvidia.GeForceExperience -e -h)
 if( $? ){
     Write-Output "Nvidia Geforce Experience installed successfully"
@@ -91,19 +123,55 @@ if( $? ){
     Write-Output "Logitech Gaming Hub installed successfully"
 }
 
-Out-Null -InputObject (winget install --id=File-New-Project.EarTrumpet -e -h)
-if( $? ){
-    Write-Output "EarTrumpet installed successfully"
-}
-
 Out-Null -InputObject (winget install --id=Discord.Discord -e -h)
 if( $? ){
     Write-Output "Discord installed successfully"
 }
 
+#--------------------------
+# Media / Audio
+#--------------------------
+Out-Null -InputObject (winget install --id=File-New-Project.EarTrumpet -e -h)
+if( $? ){
+    Write-Output "EarTrumpet installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=plex.plexmediaserver -e -h)
+if( $? ){
+    Write-Output "Plex Media Server installed successfully"
+}
+
 Out-Null -InputObject (winget install --id=plex.Plex -e -h)
 if( $? ){
     Write-Output "Plex installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=VideoLAN.VLC -e -h)
+if( $? ){
+    Write-Output "VLC installed successfully"
+}
+
+#--------------------------
+# Utils
+#--------------------------
+Out-Null -InputObject (winget install --id=Google.DriveFileStream -e -h)
+if( $? ){
+    Write-Output "Google Drive for Windows installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=Microsoft.PowerToys -e -h)
+if( $? ){
+    Write-Output "Powertoys installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=Microsoft.WindowsTerminal -e -h) 
+if( $? ){
+    Write-Output "Windows Terminal installed successfully"
+}
+
+Out-Null -InputObject (winget install --id=7zip.7zip -e -h)
+if( $? ){
+    Write-Output "7Zip installed successfully"
 }
 
 Out-Null -InputObject (winget install --id=NZXT.CAM -e -h)
@@ -114,21 +182,6 @@ if( $? ){
 Out-Null -InputObject (winget install --id=Surfshark.SurfsharkVPN -e -h)
 if( $? ){
     Write-Output "Surfshark installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=Google.AndroidStudio -e -h) 
-if( $? ){
-    Write-Output "Android Studio installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=JetBrains.IntelliJIDEA.Community -e -h)
-if( $? ){
-    Write-Output "IntelliJIDEA Community installed successfully"
-} 
-
-Out-Null -InputObject (winget install --id=Git.Git -e -h)
-if( $? ){
-    Write-Output "Git installed successfully"
 }
 
 Out-Null -InputObject (winget install --id=DelugeTeam.Deluge -e -h)
@@ -146,30 +199,10 @@ if( $? ){
     Write-Output "Putty installed successfully"
 }
 
-Out-Null -InputObject (winget install --id=Postman.Postman -e -h)
-if( $? ){
-    Write-Output "Postman installed successfully"
-}
-
-Out-Null -InputObject (winget install --id=ApacheFriends.Xampp -e -h)
-if( $? ){
-    Write-Output "Xampp installed successfully"
-}
-
 Out-Null -InputObject (winget install --id=WinSCP.WinSCP -e -h)
 if( $? ){
     Write-Output "WinSCP installed successfully"
 }
-
-Out-Null -InputObject (winget install --id=OpenJS.NodeJS -e -h)
-if( $? ){
-    Write-Output "Node.JS installed successfully"
-} 
-
-Out-Null -InputObject (winget install --id=BlenderFoundation.Blender -e -h)
-if( $? ){
-    Write-Output "Blender installed successfully"
-} 
 
 Out-Null -InputObject (winget install --id=Toinane.Colorpicker -e -h)
 if( $? ){
