c6f06a49842bcb5b02571a9a863970b390b4d1d3
raicovx <james.cranston91@gmail.com>
2021-04-26 10:06:42 +1000

    Removed pipes to shave some milliseconds

--- RaicovPC WPM clean install.ps1
+++ RaicovPC WPM clean install.ps1
@@ -1,151 +1,153 @@
-﻿
-winget install --id=Google.Chrome -e -h | Out-Null
+﻿#Some Discussion about Out-Null / Pipe performance
+#https://stackoverflow.com/questions/5260125/whats-the-better-cleaner-way-to-ignore-output-in-powershell
+
+Out-Null -InputObject (winget install --id=Google.Chrome -e -h)
 if( $? ){
     Write-Output "Google Chrome installed successfully"
 }
 
-winget install --id=Google.DriveFileStream -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Google.DriveFileStream -e -h)
 if( $? ){
     Write-Output "Google Drive for Windows installed successfully"
 }
 
-winget install --id=Microsoft.Office -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Microsoft.Office -e -h)
 if( $? ){
     Write-Output "Microsoft Office installed successfully"
 }
 
-winget install --id=Microsoft.Teams -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Microsoft.Teams -e -h)
 if( $? ){
     Write-Output "Microsoft Teams installed successfully"
 }
 
-winget install --id=Microsoft.PowerToys -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Microsoft.PowerToys -e -h)
 if( $? ){
     Write-Output "Powertoys installed successfully"
 }
 
-winget install --id=Microsoft.WindowsTerminal -e -h | Out-Null 
+Out-Null -InputObject (winget install --id=Microsoft.WindowsTerminal -e -h) 
 if( $? ){
     Write-Output "Windows Terminal installed successfully"
 }
 
-winget install --id=7zip.7zip -e -h | Out-Null
+Out-Null -InputObject (winget install --id=7zip.7zip -e -h)
 if( $? ){
     Write-Output "7Zip installed successfully"
 }
 
-winget install --id=VideoLAN.VLC -e -h | Out-Null
+Out-Null -InputObject (winget install --id=VideoLAN.VLC -e -h)
 if( $? ){
     Write-Output "VLC installed successfully"
 }
 
 
-winget install --id=Microsoft.VisualStudioCode.System-x64 -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Microsoft.VisualStudioCode.System-x64 -e -h)
 if( $? ){
     Write-Output "VS Code installed successfully"
 }
 
-winget install --id=Microsoft.VisualStudio.Enterprise -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Microsoft.VisualStudio.Enterprise -e -h)
 if( $? ){
     Write-Output "Visual Studio Enterprise installed successfully"
 }
 
-winget install --id=Nvidia.GeForceExperience -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Nvidia.GeForceExperience -e -h)
 if( $? ){
     Write-Output "Nvidia Geforce Experience installed successfully"
 }
 
-winget install --id=EpicGames.EpicGamesLauncher -e -h | Out-Null
+Out-Null -InputObject (winget install --id=EpicGames.EpicGamesLauncher -e -h)
 if( $? ){
     Write-Output "Epic Games Launcher installed successfully"
 }
 
-winget install --id=ElectronicArts.EADesktop -e -h | Out-Null
+Out-Null -InputObject (winget install --id=ElectronicArts.EADesktop -e -h)
 if( $? ){
     Write-Output "Origin installed successfully"
 }
 
-winget install --id=Valve.Steam -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Valve.Steam -e -h)
 if( $? ){
     Write-Output "Steam installed successfully"
 }
 
-winget install --id=Blizzard.BattleNet -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Blizzard.BattleNet -e -h)
 if( $? ){
     Write-Output "Battle.net installed successfully"
 }
 
-winget install --id=GOG.Galaxy -e -h | Out-Null
+Out-Null -InputObject (winget install --id=GOG.Galaxy -e -h)
 if( $? ){
     Write-Output "GOG Galaxy installed successfully"
 }
 
-winget install --id=Logitech.LGH -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Logitech.LGH -e -h)
 if( $? ){
     Write-Output "Logitech Gaming Hub installed successfully"
 }
 
-winget install --id=Discord.Discord -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Discord.Discord -e -h)
 if( $? ){
     Write-Output "Discord installed successfully"
 }
 
-winget install --id=plex.Plex -e -h | Out-Null
+Out-Null -InputObject (winget install --id=plex.Plex -e -h)
 if( $? ){
     Write-Output "Plex installed successfully"
 }
 
-winget install --id=NZXT.CAM -e -h | Out-Null
+Out-Null -InputObject (winget install --id=NZXT.CAM -e -h)
 if( $? ){
     Write-Output "NZXT CAM installed successfully"
 }
 
-winget install --id=Surfshark.SurfsharkVPN -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Surfshark.SurfsharkVPN -e -h)
 if( $? ){
     Write-Output "Surfshark installed successfully"
 }
 
-winget install --id=Google.AndroidStudio -e -h | Out-Null 
+Out-Null -InputObject (winget install --id=Google.AndroidStudio -e -h) 
 if( $? ){
     Write-Output "Android Studio installed successfully"
 }
 
-winget install --id=Git.Git -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Git.Git -e -h)
 if( $? ){
     Write-Output "Git installed successfully"
 }
 
-winget install --id=DelugeTeam.Deluge -e -h | Out-Null
+Out-Null -InputObject (winget install --id=DelugeTeam.Deluge -e -h)
 if( $? ){
     Write-Output "Deluge installed successfully"
 }
 
-winget install --id=TimKosse.FilezillaClient -e -h | Out-Null
+Out-Null -InputObject (winget install --id=TimKosse.FilezillaClient -e -h)
 if( $? ){
     Write-Output "Filezilla installed successfully"
 }
 
-winget install --id=PuTTY.PuTTY -e -h | Out-Null
+Out-Null -InputObject (winget install --id=PuTTY.PuTTY -e -h)
 if( $? ){
     Write-Output "Putty installed successfully"
 }
 
-winget install --id=Postman.Postman -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Postman.Postman -e -h)
 if( $? ){
     Write-Output "Postman installed successfully"
 }
 
-winget install --id=ApacheFriends.Xampp -e -h | Out-Null
+Out-Null -InputObject (winget install --id=ApacheFriends.Xampp -e -h)
 if( $? ){
     Write-Output "Xampp installed successfully"
 }
 
-winget install --id=WinSCP.WinSCP -e -h | Out-Null
+Out-Null -InputObject (winget install --id=WinSCP.WinSCP -e -h)
 if( $? ){
     Write-Output "WinSCP installed successfully"
 }
 
-winget install --id=Toinane.Colorpicker -e -h | Out-Null
+Out-Null -InputObject (winget install --id=Toinane.Colorpicker -e -h)
 if( $? ){
     Write-Output "ColorPicker installed successfully"
 } 
